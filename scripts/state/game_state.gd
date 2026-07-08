6db1f3ef6579f840539fcbc42f085cf708799492
thewasmans <thewasmans@gmail.com>
2026-07-08 15:04:22 +0200

    feat: add level state

--- scripts/state/game_state.gd
+++ scripts/state/game_state.gd
@@ -1,5 +1,9 @@
 extends Object
 class_name GameState
 
+var current_level: int
+var current_mini_game: int
+
 func _init(_game_data:GameData) -> void:
-	pass
+	current_level = 0
+	current_mini_game = 0
