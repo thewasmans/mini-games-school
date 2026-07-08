5e0c4434c04d25350339e9d1afd1b8a75a4f6d6a
thewasmans <thewasmans@gmail.com>
2026-07-08 18:23:16 +0200

    feat: add back button level

--- scripts/level/level.gd
+++ scripts/level/level.gd
@@ -5,6 +5,7 @@ extends Control
 @export var mini_game_container: Control
 @export var crossword_prefab: PackedScene
 @export var validate_button: Button
+@export var back_button: Button
 
 var level_index: int
 var level_data: LevelData
@@ -16,6 +17,7 @@ func initialize(_level_index: int, _level_data: LevelData, game_manager: GameMan
 	_game_manager = game_manager
 	validate_button.hide()
 	validate_button.pressed.connect(_on_validate_button_pressed)
+	back_button.pressed.connect(_on_back_button_pressed)
 	if level_data.mini_games.is_empty():
 		mini_game_label.text = "Level %d - Aucun mini-jeu" % (level_index + 1)
 		return
@@ -34,5 +36,9 @@ func _on_mini_game_completed() -> void:
 
 func _on_validate_button_pressed() -> void:
 	_game_manager.game_state.complete_level(level_index)
+	_game_manager.game_ui_manager.show_menu()
+	queue_free()
+
+func _on_back_button_pressed() -> void:
 	_game_manager.game_ui_manager.show_menu()
 	queue_free()
