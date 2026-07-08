6aee5bc7397afaca24341e7c9b3baec5691920ea
thewasmans <thewasmans@gmail.com>
2026-07-08 14:26:10 +0200

    feat: remove unused template content

--- scripts/managers/game_managers.gd
+++ scripts/managers/game_managers.gd
@@ -6,12 +6,11 @@ signal initialized
 @export var game_data: GameData
 @export_category("Managers")
 @export var game_ui_manager: GameUIManager
-@export var demo_manager: DemoManager
 
 var game_state:GameState
 var managers:Array[Manager]:
 	get:
-		return [game_ui_manager, demo_manager]
+		return [game_ui_manager]
 
 func initialize():
 	game_state = GameState.new(game_data)
